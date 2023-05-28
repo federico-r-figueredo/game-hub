a7e95c0ed342a780d7521bc203daf84ea6f0d93a
Federico R. Figueredo <federico.r.figueredo@protonmail.com>
2023-05-28 20:14:53 -0300

    Update base path

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://api.rawg.io/api",
     params: {
-        key: "2abc02c3231c428d84b269df583ded07"
+        key: "68d1f986bfc4426fb6f31762a05f77cc"
     }
 });

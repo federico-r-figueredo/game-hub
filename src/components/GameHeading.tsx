85f3a5bfe3636d185fb889992cc0906e93efb9e9
Federico R. Figueredo <federico.r.figueredo@protonmail.com>
2023-05-28 19:27:17 -0300

    Add a dynamic page heading

---
+++ src/components/GameHeading.tsx
@@ -0,0 +1,20 @@
+import { Heading } from "@chakra-ui/react";
+import { GameQuery } from "../App";
+
+interface GameHeadingProps {
+    gameQuery: GameQuery;
+}
+
+function GameHeading({ gameQuery }: GameHeadingProps) {
+    const heading = `${gameQuery.platform?.name || ""} ${
+        gameQuery.genre?.name || ""
+    } ${gameQuery.searchText}`;
+
+    return (
+        <Heading marginBottom={5} fontSize={"5xl"} as={"h1"}>
+            {heading}
+        </Heading>
+    );
+}
+
+export default GameHeading;

257434c4b33aa0d526067c530fb34ea653c819a4
Federico R. Figueredo <federico.r.figueredo@protonmail.com>
2023-05-31 07:25:14 -0300

    Adding 'with credentials' to axios

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export default axios.create({
     baseURL: "https://api.rawg.io/api",
+    withCredentials: false,
     params: {
         key: import.meta.env.VITE_RAWG_API_KEY
     }

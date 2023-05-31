04a0a3b68c25e0d9ae3b1d6c3a3069ab394d6545
Federico R. Figueredo <federico.r.figueredo@protonmail.com>
2023-05-31 07:28:43 -0300

    Adding headers to Axios

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,7 +2,10 @@ import axios from "axios";
 
 export default axios.create({
     baseURL: "https://api.rawg.io/api",
-    withCredentials: false,
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+    },
     params: {
         key: import.meta.env.VITE_RAWG_API_KEY
     }

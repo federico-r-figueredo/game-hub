6b6d17fc2437908907e365f2d92ace9cc7b31ad8
Federico R. Figueredo <federico.r.figueredo@protonmail.com>
2023-05-31 07:03:05 -0300

    Loading RAWG API key w/ env. variable

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://api.rawg.io/api",
     params: {
-        key: "8927e809ee27434595629dbe17422a90"
+        key: import.meta.env.VITE_RAWG_API_KEY
     }
 });

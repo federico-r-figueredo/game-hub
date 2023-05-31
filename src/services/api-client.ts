552cde0808c9c6dc658fdce200a061b84dc771a7
Federico R. Figueredo <federico.r.figueredo@protonmail.com>
2023-05-31 07:36:35 -0300

    Removing CORS headers

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,10 +2,6 @@ import axios from "axios";
 
 export default axios.create({
     baseURL: "https://api.rawg.io/api",
-    headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Content-Type": "application/json"
-    },
     params: {
         key: import.meta.env.VITE_RAWG_API_KEY
     }
